70b503b84e66e52fe780d8e1ece03fd9c2feca5a
Sean Barrett <seanbarrett9901@gmail.com>
2025-05-15 21:57:52 +0100

    Switch to persistent PostgreSQL database

--- database.py
+++ database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./leanlift.db"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://leanlift_db_user:xKijytwhle4zXHPnmGazqojkXoUhJhny@"
+    "dpg-d0j57k3e6dus73ai0adg-a.frankfurt-postgres.render.com/leanlift_db"
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
