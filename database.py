25713926828b35bc5c9c1c6f77ba14352ba227ab
Sean Barrett <seanbarrett9901@gmail.com>
2025-05-15 22:20:42 +0100

    Fix: use internal DB URL for Render

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Internal DB URL for Render internal communication
-DATABASE_URL = "postgresql://leanlift_db_user:xKjjytwhle42XHPnmGazqojkXoUhJnhy@dpg-d0j57k3e6dus73ai0adg-a:5432/leanlift_db"
+DATABASE_URL = "postgresql://leanlift_db_user:xk1jytwhle4zXHPmmGazqqjoXoUhIhny@dpg-d0j57k3e5dus73ai0adg-a/leanlift_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
